471f6cd60757141efd778b91730b9bd029c5c95b
Simon Brahan <tntpiranha@gmail.com>
2019-10-25 08:43:46 +0100

    Show prompt and read line from STDIN.

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::io::{self, Write};
+
+fn main() {
+    println!("Little Scheme In Rust");
+
+    loop {
+        let input = get_input();
+
+        println!("{}", input);
+    }
+}
+
+fn get_input() -> String {
+    let mut input = String::new();
+
+    print!("user> ");
+    let _ = io::stdout().flush();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Could not read line from STDIN");
+
+    input.trim().to_string()
+}
