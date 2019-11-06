150a091be36d192882b470e1ecd7fb35805c64c9
Simon Brahan <tntpiranha@gmail.com>
2019-11-06 13:58:59 +0000

    Replaced one-step parsing idea with lexing then parsing. Removed all parsing references, foxuc on lexing for now.

--- src/parser.rs
+++ src/parser.rs
@@ -1,24 +1,24 @@
 #[derive(Debug, PartialEq)]
-struct Expr {
-    items: Vec<Item>,
-}
-
-#[derive(Debug, PartialEq)]
-enum Item {
+enum LexToken {
     Num(f64),
     Symbol(String),
     String(String),
-    Expr(Expr),
+    LeftBracket,
+    RightBracket,
 }
 
-fn parse_input(input: &str) -> Result<Expr, &'static str> {
-    Ok(Expr {
-        items: vec![
-            Item::Symbol("+".to_string()),
-            Item::Num(1.0),
-            Item::Num(2.0),
-        ],
-    })
+fn lex_input(input: &str) -> Result<Vec<LexToken>, &'static str> {
+    Ok(vec![
+        LexToken::LeftBracket,
+        LexToken::Symbol("repeat".to_string()),
+        LexToken::String("scheme".to_string()),
+        LexToken::LeftBracket,
+        LexToken::Symbol("+".to_string()),
+        LexToken::Num(1.0),
+        LexToken::Num(2.0),
+        LexToken::RightBracket,
+        LexToken::RightBracket,
+    ])
 }
 
 #[cfg(test)]
@@ -26,18 +26,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parse_simple_expression() {
-        let input = "(+ 1 2)";
+    fn lex_simple_expression() {
+        let input = "(repeat \"scheme\" (+ 1 2))";
 
-        let expected_output = Expr {
-            items: vec![
-                Item::Symbol("+".to_string()),
-                Item::Num(1.0),
-                Item::Num(2.0),
-            ],
-        };
+        let expected_output = vec![
+            LexToken::LeftBracket,
+            LexToken::Symbol("repeat".to_string()),
+            LexToken::String("scheme".to_string()),
+            LexToken::LeftBracket,
+            LexToken::Symbol("+".to_string()),
+            LexToken::Num(1.0),
+            LexToken::Num(2.0),
+            LexToken::RightBracket,
+            LexToken::RightBracket,
+        ];
 
-        let actual_output = parse_input(&input).unwrap();
+        let actual_output = lex_input(&input).unwrap();
 
         assert_eq!(actual_output, expected_output);
     }
