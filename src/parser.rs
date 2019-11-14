d43880a7d84d993679d60c029f461c1fd14a2776
Simon Brahan <tntpiranha@gmail.com>
2019-11-14 13:13:21 +0000

    Empty lists and lists of strings with no escaped quotes are lexed.

--- src/parser.rs
+++ src/parser.rs
@@ -15,7 +15,19 @@ fn lex_input(input: &str) -> Result<Vec<LexToken>, &'static str> {
 
     while current_idx < input_length {
         if let Some((lexed_string, new_idx)) = lex_string(&input, current_idx) {
-            output.push(LexToken::String(lexed_string));
+            output.push(lexed_string);
+            current_idx = new_idx;
+            continue;
+        }
+
+        if let Some((lexed_left_bracket, new_idx)) = lex_left_bracket(&input, current_idx) {
+            output.push(lexed_left_bracket);
+            current_idx = new_idx;
+            continue;
+        }
+
+        if let Some((lexed_right_bracket, new_idx)) = lex_right_bracket(&input, current_idx) {
+            output.push(lexed_right_bracket);
             current_idx = new_idx;
             continue;
         }
@@ -26,7 +38,7 @@ fn lex_input(input: &str) -> Result<Vec<LexToken>, &'static str> {
     Ok(output)
 }
 
-fn lex_string(input: &str, from_idx: usize) -> Option<(String, usize)> {
+fn lex_string(input: &str, from_idx: usize) -> Option<(LexToken, usize)> {
     if input.chars().nth(from_idx).unwrap() != '"' {
         return None;
     }
@@ -37,7 +49,23 @@ fn lex_string(input: &str, from_idx: usize) -> Option<(String, usize)> {
         .take_while(|&char| char != '"')
         .collect::<String>();
 
-    Some((output.to_string(), from_idx + output.len() + 2))
+    Some((LexToken::String(output.to_string()), from_idx + output.len() + 2))
+}
+
+fn lex_left_bracket(input: &str, from_idx: usize) -> Option<(LexToken, usize)> {
+    if input.chars().nth(from_idx).unwrap() != '(' {
+        return None;
+    }
+
+    Some((LexToken::LeftBracket, from_idx + 1))
+}
+
+fn lex_right_bracket(input: &str, from_idx: usize) -> Option<(LexToken, usize)> {
+    if input.chars().nth(from_idx).unwrap() != ')' {
+        return None;
+    }
+
+    Some((LexToken::RightBracket, from_idx + 1))
 }
 
 #[cfg(test)]
@@ -45,10 +73,37 @@ mod tests {
     use super::*;
 
     #[test]
+    fn lex_brackets() {
+        let input = "()";
+
+        let expected_output = vec![LexToken::LeftBracket, LexToken::RightBracket];
+
+        let actual_output = lex_input(&input).unwrap();
+
+        assert_eq!(actual_output, expected_output);
+    }
+
+    #[test]
     fn lex_string() {
         let input = "\"scheme\"";
 
         let expected_output = vec![LexToken::String("scheme".to_string())];
+
+        let actual_output = lex_input(&input).unwrap();
+
+        assert_eq!(actual_output, expected_output);
+    }
+
+    #[test]
+    fn lex_list_of_strings() {
+        let input = "(\"little\" \"scheme\")";
+
+        let expected_output = vec![
+            LexToken::LeftBracket,
+            LexToken::String("little".to_string()),
+            LexToken::String("scheme".to_string()),
+            LexToken::RightBracket,
+        ];
 
         let actual_output = lex_input(&input).unwrap();
 
