5ed1e0779c206ee506dba549d3a97ea809267c77
Simon Brahan <tntpiranha@gmail.com>
2020-01-30 09:06:36 +0000

    Added parser skeleton and simple test.

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,30 @@
+use crate::lexer::LexToken;
+
 #[derive(Debug, PartialEq)]
 pub enum Expr {
     Num(f64),
     Symbol(String),
     String(String),
     List(Vec<Expr>),
+}
+
+pub fn parse_tokens(input: Vec<LexToken>) -> Result<Expr, &'static str> {
+    Ok(Expr::Symbol("little-schemer".to_string()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::lexer::*;
+
+    #[test]
+    fn parse_symbol() {
+        let input = vec![LexToken::Symbol("little-schemer".to_string())];
+
+        let expected_output = Expr::Symbol("little-schemer".to_string());
+
+        let actual_output = parse_tokens(input).unwrap();
+
+        assert_eq!(actual_output, expected_output);
+    }
 }
